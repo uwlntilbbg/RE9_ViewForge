ccb443da1a3ac4a896328274a8d0036856122ae7
uwlntilbbg <uwlntilbbg@users.noreply.github.com>
2026-03-08 22:31:03 +0000

    Upload Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using Plugin = RE9_ViewForge.ViewForgePlugin;
+
+
+[assembly: AssemblyTitle(Plugin.PLUGIN_NAME)]
+[assembly: AssemblyProduct(Plugin.PLUGIN_NAME)]
+[assembly: AssemblyCopyright(Plugin.COPYRIGHT)]
+[assembly: AssemblyCompany(Plugin.COMPANY)]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion(Plugin.VERSION)]
